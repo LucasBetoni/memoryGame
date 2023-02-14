d6e09b0a1a7df2f973e1ded733e85fc8883d07e9
LucasBetoni <lucasbetoni@gmail.com>
2023-02-13 23:39:16 -0300

    cookie test

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
 var names =  ['Helena', 'Alice', 'Laura', 'Maria', 'Sophia',  'Manuela', 'Maitê', 'Heitor'];
 
+function createCookie(name, value, days) {
+    var expires;
+    if (days) {
+        var date = new Date();
+        date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+        expires = "; expires=" + date.toGMTString();
+    }
+    else {
+        expires = "";
+    }
+    document.cookie = name + "=" + value + expires + "; path=/";
+}
+
+function getCookie(c_name) {
+    if (document.cookie.length > 0) {
+        let c_start = document.cookie.indexOf(c_name + "=");
+        if (c_start != -1) {
+            c_start = c_start + c_name.length + 1;
+            let c_end = document.cookie.indexOf(";", c_start);
+            if (c_end == -1) {
+                c_end = document.cookie.length;
+            }
+            return unescape(document.cookie.substring(c_start, c_end));
+        }
+    }
+    return "";
+}
+
+
+var json_str = getCookie('mycookie-memmoryGame');
+if(!json_str === undefined)	names = JSON.parse(json_str); 
+
 var symbols =  names.concat(names),
 		opened = [],
 		match = 0,
@@ -151,6 +183,9 @@ $mudarNomes.on('click', function() {
 			}
 
 			names = newNames;
+			var json_str = JSON.stringify(names);
+			createCookie('mycookie-memmoryGame', json_str);
+
 			symbols =  names.concat(names);
 			initGame();
 			swal.close();
