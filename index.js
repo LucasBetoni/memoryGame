16eec5077d9a79f5f6632888b7fc4bd4deded87f
Matheus C.R <dev@mcarvalhor.com>
2023-02-14 18:01:47 -0300

    Fixed input copy-paste.

--- index.js
+++ index.js
@@ -171,14 +171,17 @@ $share.on("click", function () {
   swal({
     title: "<strong>Compartilhe este jogo com os nomes:</strong>",
     icon: "info",
-    html: '<input type="url" name="link" value="'+outputStr+'" readonly></input>',
+    html: '<input type="text" value="'+outputStr+'" onclick="this.select();" readonly></input>',
     showCloseButton: true,
     showCancelButton: true,
     focusConfirm: false,
-    confirmButtonText: '<i class="fa fa-thumbs-up"></i> Great!',
+    confirmButtonText: '<i class="fa fa-thumbs-up"></i> Copiar',
     confirmButtonAriaLabel: "Thumbs up, great!",
     cancelButtonText: '<i class="fa fa-thumbs-down"></i>',
     cancelButtonAriaLabel: "Thumbs down",
+    preConfirm: () => {
+      copyToClipboard(outputStr);
+    }
   });
 });
 
@@ -313,6 +316,31 @@ function parseURLParams(url) {
     parms[n].push(nv.length === 2 ? v : null);
   }
   return Object.keys(parms);
+}
+
+function copyToClipboard(text) {
+  if (window.clipboardData && window.clipboardData.setData) {
+      // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
+      return window.clipboardData.setData("Text", text);
+
+  }
+  else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+      var textarea = document.createElement("textarea");
+      textarea.textContent = text;
+      textarea.style.position = "fixed";  // Prevent scrolling to bottom of page in Microsoft Edge.
+      document.body.appendChild(textarea);
+      textarea.select();
+      try {
+          return document.execCommand("copy");  // Security exception may be thrown by some browsers.
+      }
+      catch (ex) {
+          console.warn("Copy to clipboard failed.", ex);
+          return prompt("Copy to clipboard: Ctrl+C, Enter", text);
+      }
+      finally {
+          document.body.removeChild(textarea);
+      }
+  }
 }
 
 let names2 = parseURLParams(document.URL);
