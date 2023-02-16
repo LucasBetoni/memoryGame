467c073d50ccb79b0fbfd2721fdf8b50cd5be259
LucasBetoni <lucasbetoni@gmail.com>
2023-02-16 00:24:35 -0300

    updated backend address ssl

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const NAME_MAX_SIZE = 42;
 const NAME_FILTER_REGEX = /^[^()\/><\][\\\x22,;|]+$/;
 const MAX_LOADED_AUDIOS = 100;
 //const AUDIO_URL = './{name}.mp3';
-const AUDIO_URL = 'http://132.226.248.223:3000/requestaudio/{name}?key=a188022f2dc551fdb37e8a844c01dc1b';
+const AUDIO_URL = 'https://lks-oracle.cluster.mcarvalhor.com/requestaudio/{name}?key=a188022f2dc551fdb37e8a844c01dc1b';
 const EFFECT_AUDIO_VOLUME = 0.5;
 
 var namesAudio = {};
